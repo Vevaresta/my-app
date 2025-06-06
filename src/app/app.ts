7850ca203d8827b001945ce6851fa9f189420649
Vevaresta <73753308+Vevaresta@users.noreply.github.com>
2025-06-06 21:26:27 +0200

    Created ProductsModule

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+// here goes all the components that I want to appear globally (on every page)
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
