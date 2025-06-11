cb80a22b2bc3acfaff0a564b90cfbdbff109ad1f
Vevaresta <73753308+Vevaresta@users.noreply.github.com>
2025-06-11 18:02:38 +0200

    Added product list, product interace and some styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductList } from "./product-list/product-list";
 // here goes all the components that I want to appear globally (on every page)
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/product-list/product-list.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Product } from '../product';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [],
+  templateUrl: './product-list.html',
+  styleUrl: './product-list.css'
+})
+export class ProductList {
+  products: Product[] = [];
+
+}
