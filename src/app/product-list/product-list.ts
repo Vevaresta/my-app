98075a772f4d0994b7793b80b86289f4b3eecba6
Vevaresta <73753308+Vevaresta@users.noreply.github.com>
2025-06-18 11:09:29 +0200

    Created pipes for product detail

--- src/app/product-list/product-list.ts
+++ src/app/product-list/product-list.ts
@@ -14,10 +14,38 @@ export class ProductList implements AfterViewInit {
     console.log(this.productDetail()!.product());
   }
   products: Product[] = [
-    {id: 1, title: "Keyboard"},
-    {id: 2, title: "Microphone"},
-    {id: 3, title: "Web camera"},
-    {id: 4, title: "Tablet"}
+    {
+      id: 1,
+      title: "Keyboard",
+      price: 100,
+      categories: {
+        1: "Computing",
+        2: "Peripherals"
+      }
+    },
+    {
+      id: 2,
+      title: "Microphone",
+      price: 35,
+      categories: { 3: "Multimedia"
+
+      }
+    },
+    {
+      id: 3,
+      title: "Web camera",
+      price: 79,
+      categories: {
+        1: "Computing",
+        2: "Multimedia"
+      }
+    },
+    {
+      id: 4,
+      title: "Tablet",
+      price: 500,
+      categories: { 4: "Entertainment" }
+    }
 
   ];
   selectedProduct: Product | undefined = this.products[0];
