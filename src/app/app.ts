db652f897f2a734054942891f1b6b88145a626a7
Vevaresta <73753308+Vevaresta@users.noreply.github.com>
2025-06-23 19:53:18 +0200

    Created copyright directive

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductList } from "./product-list/product-list";
+import { Copyright } from './copyright';
+
 // here goes all the components that I want to appear globally (on every page)
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductList],
+  imports: [RouterOutlet, ProductList, Copyright],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
