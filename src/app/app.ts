8a10f8020e48b169ad7b329eda7738f31f76fb37
Vevaresta <73753308+Vevaresta@users.noreply.github.com>
2025-07-01 19:17:05 +0200

    made key logger component and added it to main comp

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,11 +4,12 @@ import { ProductList } from "./product-list/product-list";
 import { Copyright } from './copyright';
 import { APP_SETTINGS, appSettings, AppSettings } from './app.settings';
 import { Observable } from 'rxjs';
+import { KeyLoggerComponent } from './key-logger-component/key-logger-component';
 
 // here goes all the components that I want to appear globally (on every page)
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductList, Copyright],
+  imports: [RouterOutlet, ProductList, Copyright, KeyLoggerComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
   standalone: true,
---
+++ src/app/key-logger-component/key-logger-component.ts
@@ -0,0 +1,18 @@
+import { Component, ElementRef, OnInit, viewChild } from '@angular/core';
+import {  fromEvent } from "rxjs";
+
+@Component({
+  selector: 'app-key-logger-component',
+  imports: [],
+  templateUrl: './key-logger-component.html',
+  styleUrl: './key-logger-component.css'
+})
+export class KeyLoggerComponent implements OnInit {
+  input = viewChild<ElementRef>("keyContainer");
+  keys = "";
+
+  ngOnInit(): void {
+    const logger$ = fromEvent<KeyboardEvent>(this.input()!.nativeElement, "keyup");
+    logger$.subscribe(evt => this.keys += evt.key);
+  }
+}
