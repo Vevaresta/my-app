83393b5de0b74d1ad6fe6c54327f85c06bcf578a
Vevaresta <73753308+Vevaresta@users.noreply.github.com>
2025-07-02 18:46:53 +0200

    Made observable pipe for key logger

--- src/app/key-logger-component/key-logger-component.ts
+++ src/app/key-logger-component/key-logger-component.ts
@@ -1,5 +1,5 @@
-import { Component, ElementRef, OnInit, viewChild } from '@angular/core';
-import {  fromEvent } from "rxjs";
+import { Component, ElementRef, input, OnInit, viewChild } from '@angular/core';
+import {  filter, fromEvent, map, tap } from "rxjs";
 
 @Component({
   selector: 'app-key-logger-component',
@@ -8,11 +8,22 @@ import {  fromEvent } from "rxjs";
   styleUrl: './key-logger-component.css'
 })
 export class KeyLoggerComponent implements OnInit {
-  input = viewChild<ElementRef>("keyContainer");
-  keys = "";
+  input = viewChild<ElementRef>('keyContainer');
+  numeric = input(false);
+  keys = '';
 
   ngOnInit(): void {
-    const logger$ = fromEvent<KeyboardEvent>(this.input()!.nativeElement, "keyup");
-    logger$.subscribe(evt => this.keys += evt.key);
-  }
+    const logger$ = fromEvent<KeyboardEvent>(this.input()!.nativeElement, 'keyup');
+    
+    logger$.pipe(
+      map(evt => evt.key.charCodeAt(0)),
+      filter(code => {
+        if (this.numeric()) {
+          return (code > 31 && (code < 48 || code > 57)) === false;
+        }
+        return true;
+      }),
+      tap(digit => this.keys += String.fromCharCode(digit))
+    ).subscribe();
+  }  
 }
